481cfd7ffbe47a41d596d47a0895f4b2a5cbc04f
Sergio Londono <sergio.londono@gmail.com>
2020-10-29 11:28:03 -0500

    Sección 9 lista

--- src/app/ingreso-egreso/estadistica/estadistica.component.ts
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -30,6 +30,11 @@ export class EstadisticaComponent implements OnInit {
 
   generarEstadistica(items: IngresoEgreso[]) {
 
+    this.ingresos = 0;
+    this.egresos = 0;    
+    this.totalIngresos = 0;
+    this.totalEgresos = 0;
+
     for (const item of items) {
       if(item.tipo === 'ingreso') {
         this.totalIngresos += item.monto;
